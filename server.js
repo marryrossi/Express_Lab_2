6847360c14ab9649439dedc647d595de58740bfd
Mary Rossi <maryrossi106@gmail.com>
2022-09-17 23:27:05 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`Hello, ${req.params.name}! It's so great to see you!` )
 });
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send(`The tip amount is $${(req.params.tipPercentage / 100) * req.params.total}.`)
+});
+
 // telling app to listen on port
 app.listen(3000, function(){ 
     console.log("Express is listening for requests from the browser")
